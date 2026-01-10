27024e3129790c7720f1ac852ab6a813a2a87486
Hanice Sun <sunhanice@gmail.com>
2026-01-09 22:45:15 -0800

    hanice

--- src/hla6/array.py
+++ src/hla6/array.py
@@ -28,7 +28,7 @@ class Array():
         cmd = f'tcsh SNP2HLA.csh {in_file} {ref_file} {out_file} plink {heap_size} {window_size}'
         subprocess.run(cmd, shell=True, check=True)
 
-    def run_deephla(self, mode='train', in_file='1958BC_Pan-Asian_REF', out_file='1958BC_Pan-Asian_REF_DEEPHLA', ref_file='Pan-Asian_REF', subset=[], model_json=None, model_dir='model', deephla_dir=None):
+    def run_deephla(self, mode='train', in_file='1958BC_Pan-Asian_REF', ref_file='Pan-Asian_REF', subset=[], model_json=None, model_dir='model', deephla_dir=None):
         if not deephla_dir:
             deephla_dir = f'{resources.files("hla6").parent.parent}/vendor/DEEP-HLA'
 
@@ -63,113 +63,67 @@ class Array():
                 model_json = model_json.split('.model.json')[0]
                 hla_json = hla_json.split('.hla.json')[0]
                 cmd = f'conda run -n DEEP-HLA python {deephla_dir}/train.py --ref {ref_file} --sample {in_file} --model {model_json} --hla {hla_json} --model-dir {model_dir}'
-                print(cmd)
                 subprocess.run(cmd, shell=True, check=True)
             else:
                 raise FileNotFoundError(f'{model_json} not found')
 
         elif mode == 'impute':
             model_json = model_json.split('.model.json')[0]
-            hla_json = hla_json.split('.hla.json')[0]
-            cmd = f'conda run -n DEEP-HLA python {deephla_dir}/impute.py --sample {in_file} --model {model_json} --hla  {hla_json) --model-dir {model_dir} --out out_file'
+            hla_json = ref_file
+            cmd = f'conda run -n DEEP-HLA python {deephla_dir}/impute.py --sample {in_file} --model {model_json} --hla  {hla_json} --model-dir {model_dir} --out {in_file}'
             print(cmd)
-            #subprocess.run(cmd, shell=True, check=True)
+            subprocess.run(cmd, shell=True, check=True)
 
-    def format_output(self, in_file='data/1958BC_Euro.bgl.phased', out_file='data/1958BC_Euro_digit4.txt', digit=4, in_type='snp2hla'):
-        if in_type == 'snp2hla':
-            if in_file.endswith('.phased'):
-                header = ['SampleID', 'HLA', 'Allele1', 'Allele2']
-                df = pd.read_table(in_file, sep=' ', skiprows=1)
-                df = df.loc[df.iloc[:, 1].str.startswith('HLA'), ]
+    def format_output(self, in_file='1958BC_Euro.bgl.phased', fam='1958BC_Euro.fam', out_file='1958BC_Euro_digit4.txt', digit=4, from_tool='snp2hla'):
+        if from_tool == 'snp2hla':
+            sep = ' '
+            skiprows = 1
+            col = 1
+            in_header = 0
+        elif from_tool == 'deephla':
+            sep = '\t'
+            skiprows = 0
+            col = 0
+            in_header = None
+            samples = pd.read_table(in_file.replace('.deephla.phased', '.fam'), sep=' ', header=None).iloc[:, 1].tolist()
 
-                wh = [len(x.split('_')[2]) == digit for x in df.iloc[:, 1]]
-                df = df.loc[wh, ]
+        if in_file.endswith('.phased'):
+            df = pd.read_table(in_file, sep=sep, skiprows=skiprows, header=in_header)
+            df = df.loc[df.iloc[:, col].str.startswith('HLA'), ]
 
-                L = []
-                for n in range(2, df.shape[1], 2):
+            header = ['SampleID', 'HLA', 'Allele1', 'Allele2']
+            wh = [len(x.split('_')[2]) == digit for x in df.iloc[:, col]]
+            df = df.loc[wh, ]
+
+            L = []
+            for n in range(col + 1, df.shape[1], 2):
+                if from_tool == 'snp2hla':
                     sample_id = df.columns[n]
-                    allele1 = {}
-                    allele2 = {}
-                    for m in range(df.shape[0]):
-                        allele = df.iloc[m, 1]
-                        if digit == 4:
-                            allele = f'{allele[0:-2]}:{allele[-2:]}'
-                        fields = allele.split('_')
-                        k = '-'.join(fields[0:2])
-                        if df.iloc[m, n] == 'P':
-                            allele1.setdefault(k, [])
-                            allele1[k].append(':'.join([k] + fields[2:]))
-                        if df.iloc[m, n + 1] == 'P':
-                            allele2.setdefault(k, [])
-                            allele2[k].append(':'.join([k] + fields[2:]))
+                elif from_tool == 'deephla':
+                    sample_id = samples[int((n-col)/2)]
+                allele1 = {}
+                allele2 = {}
+                for m in range(df.shape[0]):
+                    allele = df.iloc[m, col]
+                    if digit == 4:
+                        allele = f'{allele[0:-2]}:{allele[-2:]}'
+                    fields = allele.split('_')
+                    k = '-'.join(fields[0:2])
+                    if df.iloc[m, n] == 'P':
+                        allele1.setdefault(k, [])
+                        allele1[k].append(':'.join([k] + fields[2:]))
+                    if df.iloc[m, n + 1] == 'P':
+                        allele2.setdefault(k, [])
+                        allele2[k].append(':'.join([k] + fields[2:]))
 
-                    for hla in self.HLA:
-                        L.append([sample_id, hla, ','.join(allele1.get(hla, 'X')), ','.join(allele2.get(hla, 'X'))])
+                for hla in self.HLA:
+                    L.append([sample_id, hla, ','.join(allele1.get(hla, 'X')), ','.join(allele2.get(hla, 'X'))])
 
-                df = pd.DataFrame(L)
-                df.columns = header
-                df.to_csv(out_file, header=True, index=False, sep='\t')
-                print('Formatted SNP2HLA output saved to', out_file)
+            df = pd.DataFrame(L)
+            df.columns = header
+            df.to_csv(out_file, header=True, index=False, sep='\t')
+            print('Formatted output saved to', out_file)
 
-            elif in_file.endswith('.dosage'):
-                df = pd.read_table(in_file.replace('.dosage', '.bgl.phased'), sep=' ', skiprows=1)
-                sample_name_list = [df.columns[n] for n in range(2, df.shape[1], 2)]
-                header = ['SampleID', 'HLA', 'Allele1', 'Allele2', 'Dosage1', 'Dosage2']
-                df = pd.read_table(in_file, sep='\t', header=None)
-                df = df.loc[df.iloc[:, 0].str.startswith('HLA'), ]
-
-                wh = [len(x.split('_')[2]) == digit for x in df.iloc[:, 0]]
-                df = df.loc[wh, ]
-
-                L = []
-                S = []
-                for n in range(3, df.shape[1]):
-                    D = {}
-                    sample_id = sample_name_list[n - 3]
-                    if sample_id not in S:
-                        S.append(sample_id)
-                    for m in range(df.shape[0]):
-                        allele = df.iloc[m, 0]
-                        if digit == 4:
-                            allele = f'{allele[0:-2]}:{allele[-2:]}'
-                        fields = allele.split('_')
-                        k = '-'.join(fields[0:2])
-                        D.setdefault(k, [])
-                        D[k].append([':'.join([k] + fields[2:]), df.iloc[m, n]])
-
-                    for hla in self.HLA:
-                        allele1 = 'X'
-                        allele2 = 'X'
-                        dosage1 = 'X'
-                        dosage2 = 'X'
-                        if hla in D:
-                            alleles = sorted(D[hla], key=lambda x:float(x[1]), reverse=True)
-                            if len(alleles) >= 2:
-                                if (float(alleles[0][1]) <= 1.5 and float(alleles[0][1]) > 0.5) \
-                                        and (float(alleles[1][1]) <= 1.5 and float(alleles[1][1]) > 0.5):
-                                    allele1 = alleles[0][0]
-                                    dosage1 = alleles[0][1]
-                                    allele2 = alleles[1][0]
-                                    dosage2 = alleles[1][1]
-                                elif float(alleles[0][1]) > 1.5:
-                                    allele1 = alleles[0][0]
-                                    dosage1 = alleles[0][1]
-                                    allele2 = alleles[0][0]
-                                    dosage2 = alleles[0][1]
-                            elif len(alleles) == 1:
-                                if float(alleles[0][1]) > 1.5:
-                                    allele1 = alleles[0][0]
-                                    dosage1 = alleles[0][1]
-                                    allele2 = alleles[0][0]
-                                    dosage2 = alleles[0][1]
-                                elif float(alleles[0][1]) <= 1.5 and float(alleles[0][1]) > 0.5:
-                                    allele1 = alleles[0][0]
-                                    dosage1 = alleles[0][1]
-                        L.append([sample_id, hla, allele1, allele2, dosage1, dosage2])
-                df = pd.DataFrame(L)
-                df.columns = header
-                df.to_csv(out_file, header=True, index=False, sep='\t')
-                print('Formatted SNP2HLA output saved to', out_file)
 
 if __name__ == "__main__":
     ar = Array()
--- src/hla6/cli.py
+++ src/hla6/cli.py
@@ -14,14 +14,13 @@ def get_parser():
 
     p2 = subparsers.add_parser("format-output", help="format the output to allele table")
     p2.add_argument('--input', type=str, default='data/1958BC_Euro.bgl.phased', help='input file')
-    p2.add_argument('--input_type', type=str, default='snp2hla', help='where does the input come from')
     p2.add_argument('--output', type=str, default='data/1958BC_Euro_digit4.txt', help='output file')
     p2.add_argument('--digit', type=int, default=4, help='digit level for HLA alleles')
+    p2.add_argument('--from_tool', type=str, default='snp2hla', help='the tool that generated the input file')
 
     p3 = subparsers.add_parser("run-deephla", help="run CNN-based DEEP*HLA, to be implemented")
     p3.add_argument('--mode', type=str, default='train', help='mode: train or impute')
     p3.add_argument('--input', type=str, default='1958BC_Pan-Asian_REF', help='input file prefix')
-    p3.add_argument('--output', type=str, default='1958BC_Pan-Asian_REF_DEEPHLA', help='output file prefix')
     p3.add_argument('--ref', type=str, default='Pan-Asian_REF', help='reference panel prefix, can be HM_CEU_REF or Pan-Asian_REF currently')
     p3.add_argument('--subset', type=str, default=None, help='subset the input to the HLA regions according to the reference genome, e.g., chr6:28510120-33480577 on GRCh37')
     p3.add_argument('--model_json', type=str, default='Pan-Asian_REF.model.json', help='the config file of the model')
@@ -44,9 +43,9 @@ def main():
         ar.run_snp2hla(in_file=args.input, ref_file=args.ref)
     if args.command == 'run-deephla':
         ar.run_deephla(mode=args.mode, in_file=args.input, ref_file=args.ref, subset=args.subset,
-                       model_json=args.model_json, model_dir=args.model_dir, out_file=args.output)
+                       model_json=args.model_json, model_dir=args.model_dir)
     elif args.command == 'format-output':
-        ar.format_output(in_file=args.input, in_type=args.input_type, out_file=args.output, digit=args.digit)
+        ar.format_output(in_file=args.input, out_file=args.output, digit=args.digit, from_tool=args.from_tool)
     elif args.command == 'run-xHLA':
         seq = Seq()
         seq.run_xHLA(input_bam=args.input, out_dir=args.output_dir, sample_id=args.sample_id, singularity_sif=args.singularity_sif)
